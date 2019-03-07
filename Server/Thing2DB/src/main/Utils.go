7f4ba85ff22444afeac7fef19b9c1f01fc4e7ba2
dszondy <szondy.daniel@gmail.com>
2019-03-07 10:53:22 +0100

    Some updates

--- Server/Thing2DB/src/main/Utils.go
+++ Server/Thing2DB/src/main/Utils.go
@@ -31,7 +31,7 @@ func respondOK(w http.ResponseWriter){
 	respondWithJSON(w, http.StatusOK, jsonSuccess{true})
 }
 
-func tryReadRequestBody(r *http.Request, payload interface{}, w http.ResponseWriter) bool {
+func readRequestBody(r *http.Request, payload interface{}, w http.ResponseWriter) bool {
 	decoder := json.NewDecoder(r.Body)
 	if err := decoder.Decode(payload); err != nil {
 		log.Printf("Invalid request payload: %v", err.Error())
